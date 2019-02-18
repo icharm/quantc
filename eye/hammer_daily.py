1463ef9cce145cc01c7d42764e5c112f3a37f17f
icharm_destop <icharm.me@outlook.com>
2019-02-19 00:03:22 +0800

    fix some error in hammer eye

--- eye/hammer_daily.py
+++ eye/hammer_daily.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 # Find hammer shape in daily line.
 import time
+import traceback
 from base import log
 from cninfo import cninfo
 from model.quantc import SwStock
@@ -16,37 +17,45 @@ linehead_line_ratio_h = 0.1
 def main():
     count = 0
     for stock in SwStock.select():
-        logger.debug('Start finding hammer shape...')
-        nodes = cninfo.daily_line(stock.seccode)
-        length = len(nodes)
-        result = is_hammer_shape(nodes[length - 1])
-        if not result:
+        try:
+            logger.debug('Hammer analysis in stock : ' + stock.seccode + ' ' + stock.secname)
+            nodes = cninfo.daily_line(stock.seccode)
+            # Find today node
+            now = int(time.time() * 1000)
+            today = None
+            for node in nodes:
+                balance = now - int(node[0])
+                if balance > 864000000 or balance < 0:  # Not the same day, 864000000 is the number of milliseconds a day
+                    continue
+                today = node
+            # Determine
+            result = is_hammer_shape(today)
+            if not result:
+                continue
+            HammerShape.create(
+                seccode=stock.seccode,
+                secname=stock.secname,
+                type='day',
+                color=result['color'],
+                ratio=result['ratio'],
+                lratio=result['lratio'],
+                date=time.strftime("%Y-%m-%d", time.localtime()),
+                score_s=score_s(result['ratio'], result['lratio']),
+            )
+            count += 1
+            result['seccode'] = stock.seccode
+            result['secname'] = stock.secname
+            logger.debug('Found! : ' + str(result))
+        except:
+            traceback.extract_stack()
             continue
-        HammerShape.create(
-            seccode=stock.seccode,
-            secname=stock.secname,
-            type='day',
-            color=result['color'],
-            ratio=result['ratio'],
-            lratio=result['lratio'],
-            date=time.strftime("%Y-%m-%d", time.localtime()),
-            score_s=score_s(result['ratio'], result['lratio']),
-        )
-        count += 1
-        result['seccode'] = stock.seccode
-        result['secname'] = stock.secname
-        logger.debug('Found! : ' + str(result))
-# todo entity_h 转正数
+
 def is_hammer_shape(node):
     color = 1
-    # Determine if the two timestamps are the same day
-    now = int(time.time() * 1000)
-    balance = now - int(node[0])
-    # Not the same day, 864000000 is the number of milliseconds a day
-    # if balance > 864000000 or balance < 0:
-    #     return False
+    if node[1] < 2:     # Open price < 2, not consider.
+        return False
     line_h = round(node[3] - node[4], 2)    # Keep two decimals
-    entity_h = round(node[1] - node[2], 2)
+    entity_h = abs(round(node[1] - node[2], 2))
     # rais(1) or drop(-1)
     if entity_h > 0:    # Open - Close > 0 drop
         color = -1
@@ -63,6 +72,7 @@ def is_hammer_shape(node):
     if lratio > linehead_line_ratio_h:
         return False
     # is hammer shape
+    logger.info(str(node))
     return {
         'color': color,
         'ratio': ratio,
@@ -71,8 +81,8 @@ def is_hammer_shape(node):
 
 def score_s(ratio, lratio):
     add = (ratio + lratio)
-    if add == 0:
-        return 99999.99
-    return round(10 / (ratio + lratio), 2)
+    if add == 0:    # Shape T super hammer.
+        return 100
+    return round(60 / (ratio + lratio), 2)  # 60 / max(0.6) = 100
 
 main()
