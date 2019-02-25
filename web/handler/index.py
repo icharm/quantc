f9b8907f970e40a119dcda6ae818ce42ad2c1a70
icharm <icharm.me@outlook.co>
2019-02-25 20:31:50 +0800

    clear frontend file

--- web/handler/index.py
+++ web/handler/index.py
@@ -3,5 +3,6 @@ import tornado.web
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        greeting = self.get_argument('greeting', 'Hello')
-        self.write(greeting + ', welcome you to read')
+        self.render('index.html')
+        # greeting = self.get_argument('greeting', 'Hello')
+        # self.write(greeting + ', welcome you to read')
