a8baf2e6c13b250a913a9485428ec13c7e7fbaa1
icharm <icharm.me@outlook.co>
2019-03-01 20:01:08 +0800

    hammer handler upgrade.

--- web/handler/hammer_shape.py
+++ web/handler/hammer_shape.py
@@ -5,6 +5,15 @@ from ...model.quantc import HammerShape
 
 class HammerShapeHandler(tornado.web.RequestHandler):
     def get(self):
-        today = datetime.datetime.today().strftime("%Y-%m-%d")
-        stocks = HammerShape.select().where(HammerShape.date == today)
+        date_str = self.get_argument('date', '')
+        if date_str is None or date_str == '':
+            today = datetime.datetime.today()
+            today_str = today.strftime("%Y-%m-%d")
+            if today.hour < 16: # 16 clock, the job will find new hammer today.
+                yesterday = today - datetime.timedelta(days=1)
+                yester_str = yesterday.strftime("%Y-%m-%d")
+                date_str = yester_str
+            else:
+                date_str = today_str
+        stocks = HammerShape.select().where(HammerShape.date == date_str)
         self.render('hammer_shape.html', title='HammerShape', content='', stocks=stocks)
