d34bda792769851505580417929627ec4906e27e
Yanliang.Mei <yanliang.mei@dewmobile.com>
2019-06-04 19:42:49 +0800

    venus shape list page finish.

---
+++ web/handler/venus_shape.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+import datetime
+from ...model.quantc import VenusShape
+from .base import PeeweeRequestHandler
+
+class VenusShapeHandler(PeeweeRequestHandler):
+    def get(self):
+        date_str = self.get_argument('date', '')
+        if date_str is None or date_str == '':
+            today = datetime.datetime.today()
+            today_str = today.strftime("%Y-%m-%d")
+            if today.hour < 16: # 16 clock, the job will find new hammer today.
+                yesterday = today - datetime.timedelta(days=1)
+                yester_str = yesterday.strftime("%Y-%m-%d")
+                date_str = yester_str
+            else:
+                date_str = today_str
+        stocks = VenusShape.select().where(VenusShape.date == date_str).order_by(VenusShape.score_s.desc())
+        self.render('venus_shape.html', stocks=stocks)
+
+# class HammerShapeWeeklyHandler(PeeweeRequestHandler):
+#     def get(self):
+#         date_str = self.get_argument('date', '')
+#         if date_str is None or date_str == '':
+#             today = datetime.datetime.today()
+#             date_str = today.strftime("%Y-%m-%d")
+#         stocks = HammerShapeWeek.select().where(HammerShapeWeek.date == date_str).order_by(HammerShapeWeek.score_s.desc())
+#         self.render('hammer_shape_weekly.html', stocks=stocks)
--- web/route.py
+++ web/route.py
@@ -4,6 +4,7 @@ the url structure of website
 """
 from .handler.index import IndexHandler
 from .handler.hammer_shape import HammerShapeHandler, HammerShapeWeeklyHandler
+from .handler.venus_shape import VenusShapeHandler
 from .handler.stock_data import DailyLineHandler, WeeklyLineHandler
 from .handler.stock_data import QuotesHandler
 from .handler.stock_data import TestHandler
@@ -12,6 +13,7 @@ url = [
     (r'/', IndexHandler),
     (r'/hs/d', HammerShapeHandler),
     (r'/hs/w', HammerShapeWeeklyHandler),
+    (r'/vs/d', VenusShapeHandler),
     (r'/sd/daily_line', DailyLineHandler),
     (r'/sd/weekly_line', WeeklyLineHandler),
     (r'/sd/quotes', QuotesHandler),
